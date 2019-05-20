6d73b07d6091f22faeb7a61f4fe6da6dfab2214e
nomius1990 <nomius1990@gmail.com>
2019-05-20 18:55:19 +0800

    协程
    用协程和不用协程的区别

--- src/ch16/groutine/groutine_test.go
+++ src/ch16/groutine/groutine_test.go
@@ -12,7 +12,9 @@ func TestT1(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		go func(i int) { //这种写法很重要
 			fmt.Println(i)
+			time.Sleep(time.Millisecond * 1000)
 		}(i)
 	}
-	time.Sleep(time.Millisecond * 50)
+	time.Sleep(time.Millisecond * 2000)
+
 }

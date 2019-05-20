ed0feddc5cc6a404cad616a9e4e7c259023ae09c
nomius1990 <nomius1990@gmail.com>
2019-05-20 18:53:12 +0800

    协程机制
    协程是一个更轻量级的线程
    
    在 func 前面加上go 就可以用了

---
+++ src/ch16/groutine/groutine_test.go
@@ -0,0 +1,18 @@
+package groutine_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestT1(t *testing.T) {
+	// t.Log(222)
+
+	for i := 0; i < 10; i++ {
+		go func(i int) { //这种写法很重要
+			fmt.Println(i)
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 50)
+}
